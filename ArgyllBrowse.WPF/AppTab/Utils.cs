ec05552129455e98da1746c65ffbf2a7c1e79eaf
SamLS42 <sammyls@protonmail.com>
2025-08-21 22:35:11 +0200

    adds WPF(test) proejct

---
+++ ArgyllBrowse.WPF/AppTab/Utils.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace ArgyllBrowse.WPF.AppTab
+{
+    internal class Utils
+    {
+        public static Func<object> NewItemFactory
+        {
+            get { return () => new TabContent("Introduction", new Border() { Child = new Label() { Content = "This is a new Tab" } }); }
+        }
+    }
+}
