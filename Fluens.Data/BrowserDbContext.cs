1dd5bd682ecd1d0efe20bff0905477c012ccca8b
SamLS42 <sammyls@protonmail.com>
2025-09-06 22:45:36 +0200

    feat: updates to .net 10

--- Fluens.Data/BrowserDbContext.cs
+++ Fluens.Data/BrowserDbContext.cs
@@ -5,7 +5,7 @@ namespace Fluens.Data;
 
 public class BrowserDbContext(DbContextOptions<BrowserDbContext> options) : DbContext(options)
 {
-    public DbSet<BrowserTab> Tabs { get; set; }
-    public DbSet<HistoryEntry> History { get; set; }
-    public DbSet<BrowserWindow> BrowserWindows { get; set; }
+    public DbSet<BrowserTab> Tabs { get; set; } = null!;
+    public DbSet<HistoryEntry> History { get; set; } = null!;
+    public DbSet<BrowserWindow> BrowserWindows { get; set; } = null!;
 }
